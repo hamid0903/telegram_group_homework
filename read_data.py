558ffbd95a790f75d9b0414bb80d104b506036f9
Hamid Kamalov <hamid_kamalov@mail.ru>
2022-02-02 10:02:31 +0500

    5-task002

--- read_data.py
+++ read_data.py
@@ -10,7 +10,7 @@ def read_data(file_path: str)->dict:
     
     """
     #open file
-    f=open('result.json',encoding='utf8').read()
+    f=open(file_path,encoding='utf8').read()
     data=json.loads(f)
     return data
-print(read_data('result.json'))
+print(read_data('data/result.json'))
