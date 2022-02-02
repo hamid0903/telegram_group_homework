2ada8d8d6778a04314003a7b025d937b6d0b38eb
Hamid Kamalov <hamid_kamalov@mail.ru>
2022-02-02 10:05:08 +0500

    1-task02

--- find_all_users_id.py
+++ find_all_users_id.py
@@ -1,8 +1,8 @@
 from pyexpat.errors import messages
 from read_data import read_data
 import json
-#def find_all_users_id(data: dict)->list:
-"""
+def find_all_users_id(data: dict)->list:
+    """
     This function will find all the users in the json file and return the list of users id
     
     Parameters:
@@ -10,10 +10,24 @@ import json
     Returns:
         list: List containing all the users id
     """
-    #f=open('data\result.json',encoding='utf8').read()
-    #data=json.loads(f)
-    #return len(data['id'])
-
-f=open('result.json',encoding='utf8').read()
-data=json.loads(f)
-print(data["id"])
+    msg=[]
+    masg=data['messages']
+    user_id=[]
+    idx=0
+    d={}
+    indx=-1
+    while idx<len(msg):
+        d=msg[idx]
+        for k,v in d.items():
+            if k=="actor_id":
+                user_id.append(v)
+        idx+=1
+        d={}
+        user_id2=[]
+        for item in user_id:
+            if item not in user_id2:
+                user_id2.append(item)
+                    
+    return user_id2
+data=read_data('result.json')
+print(find_all_users_id(data))
